b860f38b58a518aec4bc60cfc528d6da257d052c
happygooddays <63592518+happygooddays@users.noreply.github.com>
2021-06-11 12:17:23 +0900

    Update projectDashApp.py

--- projectDashApp.py
+++ projectDashApp.py
@@ -11,8 +11,8 @@ pd.options.plotting.backend = "plotly"
 
 app = dash.Dash(__name__)
 data = pd.read_excel('kbo2020.xlsx')
-data = data.set_index(['팀명','선수명'])
-
+#data = data.set_index(['팀명','선수명'])
+data = data.set_index(['팀명'])
 app.layout = html.Div([
     html.P("팀명:"),
     dcc.Dropdown(
@@ -31,7 +31,8 @@ app.layout = html.Div([
     Output("graph", "figure"), 
     [Input("dropdown", "value")])
 def display_graph(val):
-    fig = data.loc[val,['타율']].plot(kind='bar', title='2020년 '+ val +'선수 타율')
+    fig= px.bar(data.loc[val], x='선수명',y='타율', barmode='group', hover_data=['타수','안타','2루타','3루타','홈런'] \
+                , color='2020연봉', title='2020년 '+ val +'선수 타율')
     return fig
 
 app.run_server(debug=True)
